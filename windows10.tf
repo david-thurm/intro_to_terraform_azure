1eb09021299c7e1c03ca3a88113c3a6a67a71e9a
david-thurm <132007574+david-thurm@users.noreply.github.com>
2023-08-21 15:31:33 -0500

    Update windows10.tf

--- windows10.tf
+++ windows10.tf
@@ -1,41 +1,41 @@
 #########################
 ## Windows 10 Network ###
 #########################
-resource "azurerm_public_ip" "pubip-windows10r2" {
-  name                = "windows10r2-public-ip"
+resource "azurerm_public_ip" "pubip-windows10" {
+  name                = "windows10-public-ip"
   resource_group_name = azurerm_resource_group.rg-zerotrust.name
   location            = azurerm_resource_group.rg-zerotrust.location
   allocation_method   = "Static"
 }
 
-resource "azurerm_network_interface" "vnic-windows10r2" {
-  name                = "windows10r2-nic"
+resource "azurerm_network_interface" "vnic-windows10" {
+  name                = "windows10-nic"
   resource_group_name = azurerm_resource_group.rg-zerotrust.name
   location            = azurerm_resource_group.rg-zerotrust.location
 
   ip_configuration {
-    name                          = "windows10r2-nic-config"
+    name                          = "windows10-nic-config"
     subnet_id                     = azurerm_subnet.subnet-zerotrust.id
     private_ip_address_allocation = "Static"
     private_ip_address            = "172.22.0.8"
-    public_ip_address_id          = azurerm_public_ip.pubip-windows10r2.id
+    public_ip_address_id          = azurerm_public_ip.pubip-windows10.id
   }
 }
 #################
 ## Windows 10 ###
 #################
-resource "azurerm_virtual_machine" "vm-win10r2" {
-  name                             = "windows10r2"
+resource "azurerm_virtual_machine" "windows10" {
+  name                             = "windows10"
   resource_group_name              = azurerm_resource_group.rg-zerotrust.name
   location                         = azurerm_resource_group.rg-zerotrust.location
   vm_size                          = "Standard_DS2_v2"
-  network_interface_ids            = [azurerm_network_interface.vnic-windows10r2.id]
+  network_interface_ids            = [azurerm_network_interface.vnic-windows10.id]
   delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
 
   os_profile {
-    computer_name  = "windows10r2"
-    admin_username = "doe"
+    computer_name  = "windows10"
+    admin_username = "doecon"
     admin_password = "y&7CGB*6&fizH5ffzs7^"
   }
   os_profile_windows_config {
@@ -52,7 +52,7 @@ resource "azurerm_virtual_machine" "vm-win10r2" {
   }
 
   storage_os_disk {
-    name              = "r2-win10os-disk"
+    name              = "r2-windows10os-disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Premium_LRS"
@@ -61,4 +61,7 @@ resource "azurerm_virtual_machine" "vm-win10r2" {
   tags = {
     environment = "dev"
   }
+output "dc01_public_ip_address" {
+  value = azurerm_public_ip.windows10.*.ip_address
+}
 }
